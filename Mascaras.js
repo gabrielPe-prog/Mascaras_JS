d44566f7ff274d3b27ee139c44fd529df6c18566
Gabriel L Cordeiro <if.gabriel.else.leite@gmail.com>
2023-08-15 11:29:37 -0300

    Upando o file com as máscaras

---
+++ Mascaras.js
@@ -0,0 +1,9 @@
+/* Máscaras de CPF e CNPJ */
+function applyCpfCnpjMask(input) {
+      const value = input.value.replace(/\D/g, '');
+      if (value.length <= 11) {
+        input.value = value.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+      } else {
+        input.value = value.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, '$1.$2.$3/$4-$5');
+      }
+    }
