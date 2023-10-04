24a2ada87dc07f9e9ed407d870e5c255332fa3e6
Gabriel Leite Cordeiro <81982882+gabrielPe-prog@users.noreply.github.com>
2023-10-04 13:34:15 -0300

    Update Mascaras.js

--- Mascaras.js
+++ Mascaras.js
@@ -1,6 +1,5 @@
-/* Máscaras de CPF e CNPJ */
+/* Basta adicionar o atributo oninput="NomeDaFunction(this)" no campo do input para atribuir a function no campo*/
 
-/* Basta adicionar o atributo oninput="CpfCnpjMask(this)" no campo do input */
 function CpfCnpjMask(input) {
       const value = input.value.replace(/\D/g, '');
       if (value.length <= 11) {
@@ -10,23 +9,17 @@ function CpfCnpjMask(input) {
       }
 }
 
-/* Máscaras de CEP */
-
-/* Basta adicionar o atributo oninput="CepMask(this)" no campo do input */
 function CepMask(input) {
       const value = input.value.replace(/\D/g, '');
       input.value = value.replace(/(\d{5})(\d{3})/, '$1-$2');
 }
 
-/* Máscaras de RG */
-
-/* Basta adicionar o atributo oninput="RgMask(this)" no campo do input */
 function RgMask(input) {
       const value = input.value.replace(/\D/g, '');
       input.value = value.replace(/(\d{2})(\d{3})(\d{3})(\d{1})/, '$1.$2.$3-$4');
 }
 
-/* Máscara para campos de Valor em Reais R$ */
+
 function ReaisMask(inputElement) {
   const value = inputElement.value.replace(/\D/g, ''); // Remove todos os caracteres não numéricos
   const formattedValue = (Number(value) / 100).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
@@ -42,4 +35,19 @@ function soNumeros(input) {
 function HoraMask(input) {
     const value = input.value.replace(/\D/g, '');
     input.value = value.replace(/(\d{2})(\d{2})/, '$1:$2');
+  }
+
+function celularMask(input) {
+    const celular = input.value.replace(/\D+/g, "").slice(0, 11);
+    const tamanho = celular.length;
+
+    if (tamanho === 11) {
+        input.value = `(${celular.slice(0, 2)}) ${celular.slice(2, 7)}-${celular.slice(7)}`;
+    } else if (tamanho >= 7) {
+        input.value = `(${celular.slice(0, 2)}) ${celular.slice(2, 7)}-${celular.slice(7, 11)}`;
+    } else if (tamanho >= 2) {
+        input.value = `(${celular.slice(0, 2)}) ${celular.slice(2)}`;
+    } else {
+        input.value = `(${celular}`;
+    }
   }
